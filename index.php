c264771ac0e3db0c9de0c7c67bcf10cfa264bbfc
toure <tourekadiy@gmail.com>
2017-06-09 17:37:57 +0200

    elements for delete files

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ foreach ($array as $file) {
         <h2><?php echo basename($file, '.txt'); ?></h2>
         <p><?php echo file_get_contents('post/' . $file); ?></p>
         <form action="delete-post.php" method="POST">
-        <input type="text" name="filename" value="<?php echo $file; ?>">
+        <input type="hidden" name="filename" value="<?php echo $file; ?>">
         <input type="submit" value="delete">
         </form>
         </article>
